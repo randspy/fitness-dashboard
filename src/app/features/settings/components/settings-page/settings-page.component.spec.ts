2642b3bd96266d095e5ce6adfb751b826466879c
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-11-10 08:58:38 +0100

    feat: Changing name in the settings page

--- src/app/features/settings/components/settings-page/settings-page.component.spec.ts
+++ src/app/features/settings/components/settings-page/settings-page.component.spec.ts
@@ -1,22 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { SettingsPageComponent } from './settings-page.component';
+import { UserStore } from '../../../../core/user/user.store';
 
 describe('SettingsPageComponent', () => {
   let component: SettingsPageComponent;
   let fixture: ComponentFixture<SettingsPageComponent>;
+  let userStore: InstanceType<typeof UserStore>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SettingsPageComponent],
     }).compileComponents();
 
+    userStore = TestBed.inject(UserStore);
+
     fixture = TestBed.createComponent(SettingsPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    userStore.reset();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change the user name', () => {
+    changeName('John Doe');
+
+    submit();
+
+    const card = fixture.debugElement.query(By.css('fit-card')).nativeElement;
+    expect(card.textContent).toContain('John Doe');
+    expect(card.textContent).not.toContain('Jane Doe');
+    expect(userStore.name()).toBe('John Doe');
+    expect(errorMessageElement()).not.toContain('Name is required');
+  });
+
+  it('should show an error message if the name is invalid', () => {
+    submit();
+
+    expect(errorMessageElement()).toContain('Name is required');
+  });
+
+  it('should not show an error on initial load', () => {
+    expect(errorMessageElement()).not.toContain('Name is required');
+  });
+
+  const nameInputElement = () =>
+    fixture.debugElement.query(By.css('[formControlName="name"] input'))
+      .nativeElement;
+
+  const errorMessageElement = () =>
+    nameInputElement().parentElement?.textContent;
+
+  const formElement = () =>
+    fixture.debugElement.query(By.css('form')).nativeElement;
+
+  const submit = () => {
+    formElement().dispatchEvent(new Event('submit'));
+    fixture.detectChanges();
+  };
+
+  const changeName = (name: string) => {
+    nameInputElement().value = name;
+    nameInputElement().dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  };
 });
--- src/app/features/settings/components/settings-page/settings-page.component.ts
+++ src/app/features/settings/components/settings-page/settings-page.component.ts
@@ -1,11 +1,60 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PageSkeletonComponent } from '../../../../ui/components/page-skeleton/page-skeleton.component';
+import { UserStore } from '../../../../core/user/user.store';
+import { InputComponent } from '../../../../ui/components/input/input.component';
+import {
+  FormsModule,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ButtonComponent } from '../../../../ui/components/button/button.component';
+import { CardComponent } from '../../../../ui/components/card/card.component';
 
 @Component({
   selector: 'fit-settings-page',
   standalone: true,
-  imports: [PageSkeletonComponent],
+  imports: [
+    PageSkeletonComponent,
+    InputComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonComponent,
+    CardComponent,
+  ],
   templateUrl: './settings-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [
+    `
+      :host {
+        @apply flex flex-grow flex-col;
+      }
+    `,
+  ],
 })
-export class SettingsPageComponent {}
+export class SettingsPageComponent {
+  userStore = inject(UserStore);
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    name: [this.userStore.name(), [Validators.required]],
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.userStore.setName(this.form.value.name!);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  get isNameInvalid(): boolean {
+    return !!(this.form.get('name')?.invalid && this.form.get('name')?.touched);
+  }
+
+  get nameErrorMessage(): string {
+    return this.form.get('name')?.hasError('required')
+      ? 'Name is required'
+      : '';
+  }
+}
