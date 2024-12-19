bf8c44adda59006434b1688195e8893e1af919a5
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-12-19 09:08:03 +0100

    refactor: Updated primeng version

--- setup-jest.ts
+++ setup-jest.ts
@@ -3,3 +3,17 @@ import 'jest-preset-angular/setup-jest';
 Object.defineProperty(global.crypto, 'randomUUID', {
   value: jest.fn().mockReturnValue('test-uuid'),
 });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
-import { themeConfig } from './app.styles';
 import { ToastComponent } from './ui/components/toast/toast.component';
 
 @Component({
@@ -11,8 +9,4 @@ import { ToastComponent } from './ui/components/toast/toast.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor(private config: PrimeNGConfig) {
-    this.config.theme.set(themeConfig);
-  }
-}
+export class AppComponent {}
